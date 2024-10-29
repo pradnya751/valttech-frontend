4937f1e7bf2b150de5c8fad8282cb13d3446a4a9
krishvalttech <krishna.valttech@gmail.com>
2024-10-29 21:47:53 +0530

    init commit

---
+++ src/config/constant.js
@@ -0,0 +1,7 @@
+export const BASE_URL = '/auth/signin-1';
+export const BASE_TITLE = ' | Gradient Able Premium React + Admin Template';
+
+export const CONFIG = {
+  layout: 'vertical',
+  layoutType: 'menu-light'
+};
